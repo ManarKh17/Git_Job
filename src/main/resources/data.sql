e4030a37cce5a9f5f63191ccf94fe885ddf3254e
ManarKh17 <manar.khalifa@ensi-uma.tn>
2025-10-29 00:50:31 +0100

    update

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO country (id, name_country, capital) VALUES (1, 'Tunisia', 'Soussa');
+INSERT INTO country (id, name_country, capital) VALUES (2, 'France', 'Paris');
+INSERT INTO country (id, name_country, capital) VALUES (3, 'Italy', 'Rome');
+INSERT INTO country (id, name_country, capital) VALUES (4, 'Germany', 'Berlin');
